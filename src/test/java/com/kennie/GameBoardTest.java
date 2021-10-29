2dac187ec1eae4837fc587ed172de1877fdf5a54
CooBoo84 <kennie_svensson@hotmail.com>
2021-10-29 15:15:56 +0200

    test(GameBoardTest): add Rule 2-four neighbours

--- src/test/java/com/kennie/GameBoardTest.java
+++ src/test/java/com/kennie/GameBoardTest.java
@@ -33,4 +33,19 @@ public class GameBoardTest {
         gameBoard.nextGeneration();
         Assert.assertFalse(gameBoard.isCellAlive(0, 0));
     }
+
+    //Rule 2: Any live cell with more than three live neighbours dies, as if by overcrowding.
+    @Test
+    public void shouldBeDeadByOvercrowdingWhenLiveCellHasMoreThanThreeAliveNeighbours_fourNeighbours() {
+        GameBoard gameBoard = new GameBoard();
+        gameBoard.addCell(0, 0);
+        gameBoard.addCell(1, 0);
+        gameBoard.addCell(1, 1);
+        gameBoard.addCell(0, 1);
+        gameBoard.addCell(-1, 1);
+
+        gameBoard.nextGeneration();
+        Assert.assertFalse(gameBoard.isCellAlive(0, 0));
+
+    }
 }
