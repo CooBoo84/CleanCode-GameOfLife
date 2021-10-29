a7bbba382f07e7818c6a8e9feb627cd4cc629bd1
CooBoo84 <kennie_svensson@hotmail.com>
2021-10-29 14:11:12 +0200

    test(GameBoardTest): update addCell to work

--- src/main/java/com/kennie/GameBoard.java
+++ src/main/java/com/kennie/GameBoard.java
@@ -1,5 +1,16 @@
 package com.kennie;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class GameBoard {
 
+    Set<Cell> cells = new HashSet<>();
+    public void addCell(int x, int y) {
+        this.cells.add(new Cell(x, y));
+    }
+
+    public boolean isCellAlive(int x, int y) {
+        return cells.contains(new Cell(x, y));
+    }
 }
