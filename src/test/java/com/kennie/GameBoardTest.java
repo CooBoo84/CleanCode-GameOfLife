73f98971138ede1ab859e195fa90287a78edbb3b
CooBoo84 <kennie_svensson@hotmail.com>
2021-10-29 13:58:37 +0200

    test(GameBoardTest): add addCell

--- src/test/java/com/kennie/GameBoardTest.java
+++ src/test/java/com/kennie/GameBoardTest.java
@@ -1,5 +1,16 @@
 package com.kennie;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class GameBoardTest {
+
+    @Test
+    public void addCell(){
+        GameBoard gameBoard = new GameBoard();
+        gameBoard.addCell(0,0);
+
+        Assert.assertTrue(gameBoard.isCellAlive(0,0));
+    }
 
 }
