7acad562df73df67faf09a0fe1b137f26094fd90
CooBoo84 <kennie_svensson@hotmail.com>
2021-11-01 10:08:00 +0100

    feat(GameBoard): add nextGenereration & countNeighbours to make rule 1 to work again

--- src/main/java/com/kennie/GameBoard.java
+++ src/main/java/com/kennie/GameBoard.java
@@ -6,6 +6,18 @@ import java.util.Set;
 public class GameBoard {
 
     Set<Cell> cells = new HashSet<>();
+
+    private int[][] neighbours = {
+            {-1, -1},
+            {-1, 0},
+            {-1, 1},
+            {0, -1},
+            {0, 1},
+            {1, -1},
+            {1, 0},
+            {1, 1},
+    };
+
     public void addCell(int x, int y) {
         this.cells.add(new Cell(x, y));
     }
@@ -15,8 +27,26 @@ public class GameBoard {
     }
 
     public void nextGeneration() {
-        if(cells.size() <= 2) {
-        cells.clear();
+        Set<Cell> tmpCells = new HashSet<>();
+
+        for (Cell c : cells){
+            int n = countNeighbours(c);
+            if (n == 2 || n == 3) {
+                tmpCells.add(c);
+            }
         }
+        cells = tmpCells;
+    }
+
+    private int countNeighbours(Cell cell) {
+        int count = 0;
+
+        for (int i = 0; i < neighbours.length; i++) {
+            Cell nc = new Cell(cell.getX() + neighbours[i][0], cell.getY() + neighbours[i][1]);
+            if (cells.contains(nc)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
