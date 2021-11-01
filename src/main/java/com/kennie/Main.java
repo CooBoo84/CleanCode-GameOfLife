d2f3a739e42696fd5a075dca048a4909fb93259e
CooBoo84 <kennie_svensson@hotmail.com>
2021-11-01 11:55:57 +0100

    feat(main): add function to run the program

--- src/main/java/com/kennie/Main.java
+++ src/main/java/com/kennie/Main.java
@@ -1,7 +1,7 @@
 package com.kennie;
 
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws InterruptedException {
+        new GameOfLife().run();
     }
 }
